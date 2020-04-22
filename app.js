d2a0bae8d4e4f266fb702070e4efc3ce68d1777f
Julius Caffaro <jcaffaro@muxi.com.br>
2020-04-22 17:20:27 -0300

    solving folder structure

--- app.js
+++
@@ -1,8 +0,0 @@
-// app.js
-var express = require('express');
-var app = express();
-var db = require('./db');
-// ADD THESE TWO LINES
-var UserController = require('./user/UserController');
-app.use('/users', UserController);
-module.exports = app;
---
+++ src/app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express')
+require('./db/db')
+const userRouter = require('./routers/user')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
